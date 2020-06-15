100e94dcc23d82ad133a6d9830124c8a029d2ed0
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-06-15 13:30:39 +0100

    Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * 
+ */
+void swap (int *array, int a, int b)
+{
+    int aux;
+    aux = array[a];
+    array[a] = array[b];
+    array[b] = aux;
+}
+/**
+ * selection_sort - selection sort algo
+ * @array: array
+ * @size: size
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min;
+    for (i = 0; i < size - 1; i++)
+    {
+    min = i;
+    for(j = i + 1; j < size; j++)
+    if (array[min] > array[j])
+    min = j;
+    if(min != i)
+    {
+    swap(array, min, i);
+    print_array(array, size);
+    }
+    }
+}
