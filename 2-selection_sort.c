bcee5f42ce768a46f06fcce54c57aa4555365370
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-06-15 13:33:49 +0100

    Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,16 @@
 #include "sort.h"
 /**
- * 
+ * swap - swap
+ * @array: array
+ * @a: var
+ * @b: var
  */
-void swap (int *array, int a, int b)
+void swap(int *array, int a, int b)
 {
-    int aux;
-    aux = array[a];
-    array[a] = array[b];
-    array[b] = aux;
+int aux;
+aux = array[a];
+array[a] = array[b];
+array[b] = aux;
 }
 /**
  * selection_sort - selection sort algo
@@ -16,17 +19,17 @@ void swap (int *array, int a, int b)
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    for (i = 0; i < size - 1; i++)
-    {
-    min = i;
-    for(j = i + 1; j < size; j++)
-    if (array[min] > array[j])
-    min = j;
-    if(min != i)
-    {
-    swap(array, min, i);
-    print_array(array, size);
-    }
-    }
+size_t i, j, min;
+for (i = 0; i < size - 1; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+if (array[min] > array[j])
+min = j;
+if (min != i)
+{
+swap(array, min, i);
+print_array(array, size);
+}
+}
 }
